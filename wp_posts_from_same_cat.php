346596047ae2e67dd04566f5fa75fd9781406feb
Sergey Belyaev <sergey.belyaev@p2h.com>
2018-01-16 17:04:39 +0200

    php snippet added

---
+++ wp_posts_from_same_cat.php
@@ -0,0 +1,28 @@
+<?php
+	$cats_ids = array();
+	if ( $categories = get_the_category() ) {
+		foreach ( $categories as $cat ) {
+			$cats_ids[] = $cat->term_id;
+		}
+	}
+	$args = array(
+		'posts_per_page' => 3,
+		'post__not_in' => array( get_the_ID() ),
+	);
+	if ( $cats_ids ) {
+		$args['category__in'] = $cats_ids;
+	}
+	query_posts( $args );
+?>
+<?php if ( have_posts() ) : ?>
+	<section class="related-posts">
+		<div class="container">
+			<h2><?php _e( 'Related Posts', 'base' ); ?></h2>
+			<div class="posts-list">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'blocks/content', get_post_type() ); ?>
+				<?php endwhile; ?>
+			</div>
+		</div>
+	</section>
+<?php endif; wp_reset_query(); ?>
